17955cea9ea9e5d544754382c42839fbe8e5586f
johngaynor <jgaynr@icloud.com>
2025-05-28 19:18:02 -0400

    changed logging

--- middleware/apiKey.js
+++ middleware/apiKey.js
@@ -4,10 +4,12 @@ function apiKeyMiddleware(req, res, next) {
   const userKey = req.header("x-api-key");
   const apiKey = process.env.API_KEY;
 
-  if (!userKey || userKey !== validKey) {
+  console.log("Received API Key:", userKey);
+  console.log("Expected API Key:", apiKey);
+
+  if (!userKey || userKey !== apiKey) {
     return res.status(401).json({
       error: "Unauthorized: Invalid or missing API key",
-      req,
     });
   }
 
