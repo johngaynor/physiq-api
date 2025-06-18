112971514cb32460adb26847ee3f737f758bd140
johngaynor <jgaynr@icloud.com>
2025-06-18 14:42:40 -0400

    changed db config

--- config/index.js
+++ config/index.js
@@ -6,6 +6,11 @@ const config = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQL_PORT,
+    waitForConnections: true,
+    queueLimit: 0,
+    connectTimeout: 10000,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
   },
   ouraIntegrationApiKey: process.env.OURA_INTEGRATION_API_KEY,
 };
