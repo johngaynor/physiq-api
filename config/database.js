b9d8f57c68ef5866e9010ec6511e206884eced9e
johngaynor <jgaynr@icloud.com>
2025-06-19 09:49:43 -0400

    Trying new wrapper for querying

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2/promise");
 const config = require("./index");
 
-const mysqlPool = mysql.createPool(config.mySQLConfig);
+const pool = mysql.createPool(config.mySQLConfig);
 
-mysqlPool
+pool
   .getConnection()
   .then((conn) => {
     console.log(`Connected to ${config.mySQLConfig.database} Database`);
@@ -13,4 +13,18 @@ mysqlPool
     console.error("Initial MySQL connection failed:", err);
   });
 
-module.exports = mysqlPool;
+async function query(query, params = []) {
+  const connection = await pool.getConnection();
+  try {
+    await connection.ping(); // optional but helps detect stale sockets
+    console.log("Successfully pinged the database");
+    return await connection.query(query, params);
+  } catch (err) {
+    console.error("Query failed:", err);
+    throw err;
+  } finally {
+    connection.release();
+  }
+}
+
+module.exports = { pool, query };
