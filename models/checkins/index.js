bb12224c7e99080d8477fdf69adec7ddf765fd79
johngaynor <jgaynr@icloud.com>
2025-06-27 15:10:02 -0400

    fixed table returned columns

--- models/checkins/index.js
+++ models/checkins/index.js
@@ -4,13 +4,57 @@ const checkInFunctions = {
   async getCheckIns(userId) {
     return new Promise(async function (resolve, reject) {
       try {
-        const [result] = await db.query(
+        const [checkIns] = await db.query(
           `
-           select * from checkIns where userId = (select id from apiUsers where clerkId = ?)
+           SELECT
+               id,
+               date,
+               hormones,
+               phase,
+               timeline,
+               cheats,
+               comments,
+               training,
+               cast(avgTotalSleep as double) as avgTotalSleep,
+               cast(avgTotalBed as double) as avgTotalBed,
+               cast(avgRecoveryIndex as double) as avgRecoveryIndex,
+               cast(avgRemQty as double) as avgRemQty,
+               cast(avgDeepQty as double) as avgDeepQty
+           FROM checkIns 
+           WHERE userId = (SELECT id FROM apiUsers WHERE clerkId = ?)
         `,
           [userId]
         );
-        resolve(result);
+
+        const [attachments] = await db.query(
+          `
+            SELECT
+                att.id,
+                att.checkInId,
+                att.s3Filename,
+                att.poseId
+            FROM attachments att
+            LEFT JOIN checkIns ci
+                ON ci.id = att.checkInId
+            WHERE ci.userId = (SELECT id FROM apiUsers WHERE clerkId = ?)
+          `,
+          [userId]
+        );
+
+        const checkInsWithAttachments = checkIns.map((checkIn) => {
+          return {
+            ...checkIn,
+            attachments: attachments
+              .filter((att) => att.checkInId === checkIn.id)
+              .map((att) => ({
+                id: att.id,
+                s3Filename: att.s3Filename,
+                poseId: att.poseId,
+              })),
+          };
+        });
+
+        resolve(checkInsWithAttachments);
       } catch (error) {
         reject(error);
       }
