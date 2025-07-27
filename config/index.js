ad653fa7f853d0aa61dfed85d2acf32e68b6b725
johngaynor <jgaynr@icloud.com>
2025-07-27 13:28:54 -0400

    trying new mysql config

--- config/index.js
+++ config/index.js
@@ -4,12 +4,13 @@ const config = {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+    port: Number(process.env.MYSQL_PORT),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    connectTimeout: 10000,
-    idleTimeout: 60000,
+    connectTimeout: 10000, // 10 seconds to connect
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000, // 10 seconds before first keep-alive packet
   },
   ouraIntegrationApiKey: process.env.OURA_INTEGRATION_API_KEY,
 };
