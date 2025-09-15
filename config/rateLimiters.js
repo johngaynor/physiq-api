228f4094caf58f6e03dbb8634ca1242879c1a7b5
johngaynor <john.gaynor@irvmat.com>
2025-09-14 21:51:07 -0400

    updated rate limit

--- config/rateLimiters.js
+++ config/rateLimiters.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Simple global rate limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per windowMs
+  limit: 1000, // Limit each IP to 1000 requests per windowMs
   message: {
     error: 'Too many requests from this IP, please try again later.',
   },
